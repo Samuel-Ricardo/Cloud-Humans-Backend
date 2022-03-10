6fdffe832cca8409cf7e11ab708867abf9d63acc
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-03-10 12:34:06 -0300

    Projects

---
+++ src/storage/projects.ts
@@ -0,0 +1,20 @@
+import { Project } from "@cloud/model";
+
+
+const projects: Project[] = [
+  {
+    title: "Calculate the Dark Matter of the universe for Nasa",
+    score: 10,
+  },{
+    title: "Determine if the Schrodinger's cat is alive",
+    score: 5,
+  },{
+    title: "Attend to users support for a YXZ Company",
+    score: 3,
+  },{
+    title: "Collect specific people information from their social media for XPTO Company",
+    score: 2,
+  }
+]
+
+export default projects;
