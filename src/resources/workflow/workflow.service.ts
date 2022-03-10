729b9355f8ea046b019977108d679a1347ba0bab
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-03-10 15:31:08 -0300

    Create: Service - Workflow

--- src/resources/workflow/workflow.service.ts
+++ src/resources/workflow/workflow.service.ts
@@ -1,4 +1,5 @@
 import { User, Work } from "@cloud/model";
+import { _Response } from "@cloud/API";
 import { educational_levels } from "../../config/educational_level";
 import projects from "../../storage/projects";
 
@@ -40,8 +41,35 @@ function generateScore(user: User): number {
 }
 
 
-export default class PixService {
+export default class WorkflowService {
 
+  async match(value: number, user: User): Promise<_Response<Work>> {
 
+    const work: Work = {} as Work;
+    work.score = generateScore(user);
 
+    projects.forEach(( project, index) => {
+      work.score >= project.score
+        ? work.eligible_projects.push(project)
+        : work.ineligible_projects.push(project)
+    })
+
+    work.eligible_projects.forEach(
+      (project, index, all) => {
+
+        project.score >= all[index].score
+          ?work.selected_project = project
+          :false
+      }
+    )
+
+    const response: _Response<Work> = {
+      data: work,
+      error: false,
+      message: `Your selected project is ${work.selected_project}`,
+      status: 200,
+    };
+
+    return response;
+  }
 }
