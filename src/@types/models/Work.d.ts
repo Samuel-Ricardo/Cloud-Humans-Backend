34231fc2d0418e4b21a1ca2a0b67175acbd3e9b0
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-03-10 12:38:18 -0300

    Create: Model Type - Work (./src/@types/models)

---
+++ src/@types/models/Work.d.ts
@@ -0,0 +1,10 @@
+declare module "@cloud/model" {
+
+  export interface Work {
+    score: number,
+    selected_project: Project,
+    eligible_projects: Project[],
+    ineligible_projects: []
+  }
+
+}
