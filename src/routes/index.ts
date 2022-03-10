34302a0436ad2286a6ca2ddfd9cf3de926ebc5e3
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-03-10 14:20:05 -0300

    update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import workflow_routes from "./Workflow/workflow.routes";
 
 const routes = Router();
 
 routes.use("/", (req, res) => {
   res.status(200).send({message: "Welcome to API"})
 })
+
+routes.use("/workflow", workflow_routes)
 
 export default routes;
