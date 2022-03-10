815b4c263d814272901503dfff59907992517694
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-03-10 17:55:47 -0300

    bugfix: select project

--- src/resources/workflow/workflow.service.ts
+++ src/resources/workflow/workflow.service.ts
@@ -8,8 +8,16 @@ export default class WorkflowService {
 
   async match(user: User): Promise<_Response<Work>> {
 
-    const work: Work = {} as Work;
+    const work: Work = {
+      eligible_projects: [{}],
+      ineligible_projects: [{}],
+      score: 0,
+      selected_project:{}
+    } as Work;
     work.score = generateScore(user);
+
+    work.eligible_projects.pop();
+    work.ineligible_projects.pop();
 
     projects.forEach(( project, index) => {
       work.score >= project.score
@@ -17,10 +25,12 @@ export default class WorkflowService {
         : work.ineligible_projects.push(project)
     })
 
+    work.selected_project = work.eligible_projects[0]
+
     work.eligible_projects.forEach(
       (project, index, all) => {
 
-        project.score >= all[index].score
+        project.score > work.selected_project.score
           ?work.selected_project = project
           :false
       }
@@ -29,7 +39,7 @@ export default class WorkflowService {
     const response: _Response<Work> = {
       data: work,
       error: false,
-      message: `Your selected project is ${work.selected_project}`,
+      message: `Your selected project is ${work.selected_project.title}`,
       status: 200,
     };
 
