e67b57d2cadc520750fd0ac59ba70686d10374b4
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-03-10 11:59:02 -0300

    Create: Middleware - global_error

--- src/@types/error/AppError.d.ts
+++ src/@types/error/AppError.d.ts
@@ -1,13 +1,13 @@
-declare module '@types/error'{
+declare module '@cloud/error' {
   export class AppError {
     public readonly message: string;
-  public readonly statusCode: number;
-  public readonly data?: any;
+    public readonly statusCode: number;
+    public readonly data?: any;
 
-  constructor(message: string, statusCode = 400, data?: any) {
-    this.message = message,
-    this.statusCode = statusCode,
-    this.data = data;
-  }
+    constructor(message: string, statusCode = 400, data?: any) {
+      this.message = message,
+        this.statusCode = statusCode,
+        this.data = data;
+    }
   }
 }
